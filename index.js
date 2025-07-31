fc71f65040082d307e3f7b162ff41a9b93944ec5
ekkx <geniusnaru@gmail.com>
2025-07-31 15:01:50 +0900

    chore: update progress bar design

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const generateProgressBar = () => {
-    const progressBarCapacity = 30
-    const passedProgressBarIndex = Math.floor(progressOfYear * progressBarCapacity)
-    const progressBar = '█'.repeat(passedProgressBarIndex) + '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
-    return `{ ${progressBar} }`
-}
+const generateProgressBar = (progressOfYear, progressBarCapacity = 30) => {
+	const passedBars = Math.floor(progressOfYear * progressBarCapacity);
+	const remainingBars = progressBarCapacity - passedBars - 1;
+	const progressBar =
+		"=".repeat(passedBars) + ">" + ".".repeat(Math.max(remainingBars, 0));
+	return `[${progressBar}]`;
+};
 
 const thisYear = new Date().getFullYear();
 const startTimeOfYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime();
 const endTimeOfYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime();
-const progressOfYear = (Date.now() - startTimeOfYear) / (endTimeOfYear - startTimeOfYear);
-const progressBarOfYear = generateProgressBar();
+const now = Date.now();
+const progressOfYear =
+	(now - startTimeOfYear) / (endTimeOfYear - startTimeOfYear);
+
+const dayOfYear =
+	Math.floor((now - startTimeOfYear) / (1000 * 60 * 60 * 24)) + 1;
+const totalDaysInYear =
+	Math.floor((endTimeOfYear - startTimeOfYear) / (1000 * 60 * 60 * 24)) + 1;
+
+const progressBarOfYear = generateProgressBar(progressOfYear);
 
 const readme = `\
 <pre>
@@ -22,10 +31,10 @@ const readme = `\
 │
 └───────────────────────────────▰▰▰
 
-⏳ ${new Date().getFullYear()}: ${progressBarOfYear} ${(progressOfYear * 100).toFixed(2)} %
-
-updated on ${new Date().toDateString().toLowerCase()}
+${thisYear}: ${progressBarOfYear} ${(progressOfYear * 100).toFixed(
+	1
+)} %（${dayOfYear}/${totalDaysInYear}）
 </pre>
-`
+`;
 
-console.log(readme)
+console.log(readme);
